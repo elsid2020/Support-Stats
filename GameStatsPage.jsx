ae57d73dee4d22afd50c7029762148ee3ca25a01
Elsid2020 <pentester2019@gmail.com>
2026-09-09 10:09:40 -0500

    Version 1.11.0

--- GameStatsPage.jsx
+++ GameStatsPage.jsx
@@ -1,4 +1,4 @@
-const extensionVersion = "1.10.1";
+const extensionVersion = "1.11.0";
 const React = require('react');
 const { useSelector, useDispatch } = require('react-redux');
 const { actions, selectors, util, fs, MainPage, log, Icon, IconButton, Toggle, Spinner, calculateFolderSize, OptionsFilter } = require('vortex-api');
