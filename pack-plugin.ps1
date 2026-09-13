36c286e80364caa83486fff688e93b16c77c3038
Elsid2020 <pentester2019@gmail.com>
2026-09-13 12:57:52 -0500

    packing script

---
+++ pack-plugin.ps1
@@ -0,0 +1,23 @@
+$ver = ((Get-Content "$PSScriptRoot\info.json" -Raw | ConvertFrom-Json).version) -replace '\.', '-'
+$base = "Support Stats-$ver"
+$dst = "E:\my_extensions\$base.zip"
+$i = 1
+while (Test-Path $dst) {
+    $dst = "E:\my_extensions\$base-$i.zip"
+    $i++
+}
+
+$paths = @(
+    "$PSScriptRoot\index.js",
+    "$PSScriptRoot\info.json",
+    "$PSScriptRoot\helmet1.svg",
+    "$PSScriptRoot\icon.svg",
+    "$PSScriptRoot\GameStatsPage.jsx"
+)
+
+if (Test-Path "$PSScriptRoot\assets") {
+    $paths += "$PSScriptRoot\assets\*"
+}
+
+Compress-Archive -Path $paths -DestinationPath $dst -Force
+Write-Host "Created: $dst"
